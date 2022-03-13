c554b1717ab2f964ce9209cc6d6aa2ec0a92434b
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-13 20:24:18 +0700

    Handling Get Request

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
     res.status(200).json({ Hello: "Hello World" });
 });
 
 app.post("/", (req, res) => {
     res.status(200).json({ data: "Data Received Successfully" });
+}); */
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
 });
 
 const port = 3000;
